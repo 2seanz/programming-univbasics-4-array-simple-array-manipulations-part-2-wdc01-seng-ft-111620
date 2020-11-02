d95d8a462f54801148e378cca4bf8458577fb8a7
confident-collaborator-2097 <seanzngo@gmail.com>
2020-11-02 17:42:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
 more_favs = ["sports cars", "flatiron school"]
- @all_my_favs = @my_favorite_things.concat
+ @all_my_favs = @my_favorite_things.concat(more_favs)
   
   
